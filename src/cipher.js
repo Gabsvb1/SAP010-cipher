62abb18da7fda7baea95fdaa34cd96a5f989088d
Gabriella Vieira Bruno <gabi_bvieira@hotmail.com>
2023-07-29 09:23:37 -0300

    criação da funcionalidade de encode e decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,40 @@
 const cipher = {
+  encode: function encode(offset, string) {
+    const resultado = [];
+    const upperString = string.toUpperCase();
+
+    for (let index = 0; index < upperString.length; index++) {
+      const charcode = upperString.charCodeAt(index);
+
+      if (upperString[index] === " ") {
+        resultado.push(upperString[index]);
+      } else {
+        resultado.push(
+          String.fromCharCode(((charcode - 65 + offset) % 26) + 65)
+        );
+      }
+    }
+
+    return resultado.join("");
+  },
+  decode: function decode(offset, string) {
+    const resultado = [];
+    const upperString = string.toUpperCase();
+
+    for (let index = 0; index < upperString.length; index++) {
+      const charcode = upperString.charCodeAt(index);
+
+      if (upperString[index] === " ") {
+        resultado.push(upperString[index]);
+      } else {
+        resultado.push(
+          String.fromCharCode((charcode + 65 - offset) % 26 + 65)
+        );
+      }
+    }
+
+    return resultado.join("");
+  },
   // ...
 };
 
